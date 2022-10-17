75efcd04db08b28a83d93c285a2546837f48346a
yangzkiller <clisesque2k18@gmail.com>
2022-10-17 00:10:13 -0300

    Add Loop e menu

--- AnoBissexto/README.md
+++
@@ -1,5 +0,0 @@
- Programa que peça um número correspondente a um determinado ano
- e em seguida informe se este ano é ou não bissexto. 
- 
-
- 
--- AnoBissexto/src/v1/main.java
+++ AnoBissexto/src/v1/main.java
@@ -7,19 +7,61 @@ import java.util.Scanner;
  * e em seguida informe se este ano é ou não bissexto. 
  */
 public class main {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         //OBJETO TIPO SCANNER
         Scanner entrada = new Scanner(System.in);
+
         //VARIAVEIS
         int ano;
-        //COMANDOS DE SAIDA/ENTRADA DE DADOS
-        System.out.print("Ano: ");
-        ano = entrada.nextInt();
-        //ESTRUTURA CONDICIONAL
-        if(ano % 4 == 0) {
-            System.out.println("É bissexto!");       
-        }else {
-            System.out.println("Não é bissexto!");
-        }
+        int opcao;
+
+        //DISPLAY
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        System.out.println("  --> CALCULADORA DE ANO BISSEXTO <--");
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        
+        //LAÇO
+        boolean sair = true;
+        while (sair == true) {
+
+            //MENU
+            System.out.println("             ||   MENU   ||");
+            System.out.println("----------------------------------------");
+            System.out.println("[1] Calculo Bissexto\n[2] Sair");
+            System.out.println(" ");
+            //ENTRADA DE DADOS
+            System.out.print("Digite uma opção: ");
+            opcao = entrada.nextInt();
+            System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+            
+            //ESTRUTURA CONDICIONAL - MENU
+            //OPÇÃO - 1
+            if (opcao == 1) {
+                //ENTRADA DE DADOS
+                System.out.print("Digite o Ano: ");
+                ano = entrada.nextInt();
+                System.out.println("----------------------------------------");
+
+                //PROCESSAMENTO DE DADOS
+                if(ano % 4 == 0) {
+                    System.out.println("--> Bissexto <--");
+                    System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");      
+                }else {
+                    System.out.println("--> Não é Bissexto <--");
+                    System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                }
+            
+            //OPÇÃO - 2
+            }else if (opcao == 2) {
+                System.out.println("      --> PROGRAMA ENCERRADO <--");
+                System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                sair = false;
+            
+            //ERRO
+            }else {
+                System.out.println("        !ERRO! Entrada Inválida");
+                System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+            }
+        }       
     } 
 }
