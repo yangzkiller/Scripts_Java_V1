ebcb56afacce35ff7856b1611bce80af30d93661
yangzkiller <clisesque2k18@gmail.com>
2022-10-18 13:09:18 -0300

    Update

--- CalculosGeometricos/src/Main_Geometrico.java
+++ CalculosGeometricos/src/Main_Geometrico.java
@@ -29,7 +29,7 @@ public class Main_Geometrico {
             //MENU
             System.out.println("             ||   MENU   ||");
             System.out.println("----------------------------------------");
-            System.out.println("[1] Calcular a área de um quadrado\n[2] Calcular o raio de um circulo\n[3] Calcular lados de um triângulo\n[4] Sair");
+            System.out.println("[1] Calcular a área de um quadrado\n[2] Calcular a área de um circulo\n[3] Calcular lados de um triângulo\n[4] Sair");
             System.out.println(" ");
             //ENTRADA DE DADOS
             System.out.print("Digite uma opção: ");
@@ -39,15 +39,69 @@ public class Main_Geometrico {
             //ESTRUTURA CONDICIONAL - MENU
             //OPÇÃO - 1
             if (opcao == 1) {
-                System.out.println("1");
+                //COMANDO DE ENTRADA/SAIDA DE DADOS
+                System.out.print("Digite o tamanho do lado do quadrado: "); 
+                lado = entrada.nextFloat();
+                System.out.println("----------------------------------------"); 
+        
+                //VARIAVEL AREA RECEBE VALOR DE LADO MULTIPLICADO POR ELE MESMO
+                area = lado * lado;
+        
+                //COMANDO DE SAIDA DE DADOS
+                System.out.format("A área do quadrado: %.2f \n", area);
+                System.out.format("O dobro da área do quadrado: %.2f \n", area*2);
+                System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
 
             //OPÇÃO - 2
             }else if (opcao == 2) {
-                System.out.println("2");
+                //COMANDO DE ENTRADA/SAIDA DE DADOS
+                System.out.print("Digite o raio do circulo: "); 
+                raio = entrada.nextFloat(); 
+                System.out.println("----------------------------------------"); 
+                        
+                //A VARIAVEL AREA RECEBE O VALOR DE PI
+                //MULTIPLICADO PELO RAIO MULTIPICADO POR ELE MESMO.
+                area = (float) (PI * raio * raio);
+        
+                //COMANDO DE SAIDA DE DADOS
+                System.out.printf("A área do circulo é: %.2f \n", area);
+                System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
 
             //OPÇÃO - 3
             }else if (opcao == 3) {
-                System.out.println("3");
+                //COMANDOS DE SAIDA/ENTRADA DE DADOS
+                System.out.println("---------Digite o valor dos lados---------");
+                System.out.print("lado - 1: ");
+                lado = entrada.nextInt();
+        
+                System.out.print("lado - 2: ");
+                lado_2 = entrada.nextInt();
+        
+                System.out.print("lado - 3: ");
+                lado_3 = entrada.nextInt();
+                System.out.println("----------------------------------------");
+
+                //ESTRUTURA CONDICIONAL
+                if (lado+lado_2> lado_3 && lado+lado_3> lado_2 && lado_2+lado_3> lado) {
+                    System.out.println("Os três lados formam um triângulo!");
+                    System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                }
+                else {
+                    System.out.println("Os três lados NÃO formam um triangulo"); 
+                    System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");        
+                }
+                if(lado==lado_2 && lado==lado_3) {
+                    System.out.println("Do tipo Equilatero");
+                    System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                }
+                if (lado==lado_2 && lado==lado_3 && lado_2==lado_3) {
+                    System.out.println("Do tipo Isoceles");
+                    System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                }
+                if (lado!=lado_2  && lado!=lado_3 && lado_2!=lado_3) {
+                    System.out.println("Do tipo Escaleno");
+                    System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                }
         
             //OPÇÃO - 4    
             }else if (opcao == 4) {
@@ -60,7 +114,6 @@ public class Main_Geometrico {
                 System.out.println("        !ERRO! Entrada Inválida");
                 System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
             }
-
         }
     }
 }
